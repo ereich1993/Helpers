cc7225f8951a8793887ed7fbc1d104d2bcae3003
emil <emil@DESKTOP-G1JCM4K>
2019-02-04 23:08:46 +0100

    Helpers

--- Helpers/Program.cs
+++ Helpers/Program.cs
@@ -1,12 +1,92 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Helpers
 {
-    class Program
+   public class Program
     {
+        public double TimeAtOffice { get; set; } = 0;
+        public List<TimeEvents> events { get; set; } = new List<TimeEvents>
+        {
+            new TimeEvents(DateTime.Now.AddHours(-12),leaveType.arrive),
+            new TimeEvents(DateTime.Now.AddHours(-8),leaveType.leave),
+            new TimeEvents(DateTime.Now.AddHours(-7),leaveType.arrive),
+            new TimeEvents(DateTime.Now.AddHours(-5),leaveType.leave)
+            
+        };
+
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program program = new Program();
+            Console.WriteLine(program.CalculateOfficeTime());
+            
+            Console.ReadLine();
         }
+
+        public double CalculateOfficeTime()
+        {
+            double officeTime = 0;
+            var current = new TimeEvents();
+            foreach (var t in events)
+            {
+                if (t.type==leaveType.leave&&current.type==leaveType.arrive)
+                {
+                    officeTime += CalculateTimeBetween(current.time, t.time );
+                    current = t;
+                    Console.WriteLine(officeTime);
+                }
+                if (t.type==leaveType.arrive)
+                {
+                    current = t;
+                };
+
+                
+            }
+            return officeTime;
+        }
+
+        static bool Compare()
+        {
+            Console.WriteLine(DateTime.Now.ToString("MM:dd:yyyy")); 
+            return true;
+        }
+
+        public static bool IsSameDay(DateTime date)
+        {
+            return DateTime.Now.ToString("MM:dd:yyyy") == date.ToString("MM:dd:yyyy");
+        }
+
+        public static double CalculateTimeBetween(DateTime first, DateTime second)
+        {
+            return second.Subtract(first).TotalHours;
+        }
+
+        public class TimeEvents
+        {
+            public DateTime time { get; set; }
+            public leaveType type { get; set; }
+            public TimeEvents()
+            {
+
+            }
+
+            public TimeEvents(DateTime time, leaveType type)
+            {
+                this.time = time;
+                this.type = type;
+            }
+        }
+
+        public enum leaveType
+        {
+            arrive,
+            leave
+        }
+
+
+        
+
+
     }
 }
